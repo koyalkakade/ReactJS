aecb58aaf5e972ae50ce1b4c5d05772be88c502f
Koyal Nimbalkar <koyalkakade@gmail.com>
2026-05-11 21:58:11 +0530

    add useRef hook

--- my-first-project/src/components/Counter.jsx
+++ my-first-project/src/components/Counter.jsx
@@ -1,32 +1,54 @@
 import React, { useState } from 'react'
+import { useMemo } from 'react'
+import { useEffect } from 'react'
+import { useRef } from 'react'
 
 const Counter = () => {
 
-    const [count,setCount] = useState(100)
+    const [count, setCount] = useState(100)
+    const [theme, setTheme] = useState('light')
+    const prevCount = useRef()
 
-function handleIncreament(){
-    setCount((c)=>c+1)
-}
+    useEffect(() => {
+        prevCount.current = count
+    }, [count])
 
-const handleDecreament = ()=>{
-    setCount(c=>c-1)
-}
+    function handleIncreament() {
+        setCount((c) => c + 1)
+    }
 
+    const handleDecreament = () => {
+        setCount(c => c - 1)
+    }
 
+    function expensiveCalculation(number) {
+        for (let i = 0; i <= 10; i++) { console.log('calculllll') }
+        return number * 2
+    }
+    const result = useMemo(() => {
+        return expensiveCalculation(count)
+    }, [count])
 
-  return (
-    <div style={{height:"400px"}}>
-        
-       <h1> Counter : {count}</h1>
+    function handdleTheme() {
+        setTheme(prev => prev == 'light' ? 'dark' : 'light')
+    }
+    return (
+        <div style={{ height: "400px" }}>
 
-       <button onClick={()=>setCount(count+1)}>Increament</button>
-       <button onClick={()=>setCount(count-1)}>Decreament</button>
-        <button onClick={()=>setCount(100)}>Reset</button>
-        <hr />
-        <button onClick={handleIncreament}>Increament By Function</button>
-        <button onClick={handleDecreament}>Decreament By Function</button>
+            <h1> Counter : {count}</h1>
+            <h2>Previous Count:{prevCount.current}</h2>
+            <h3>Result for expensive calculation:{result}</h3>
+            <button onClick={() => setCount(count + 1)}>Increament</button>
+            <button onClick={() => setCount(count - 1)}>Decreament</button>
+            <button onClick={() => setCount(100)}>Reset</button>
+            <hr />
+            <button onClick={handleIncreament}>Increament By Function</button>
+            <button onClick={handleDecreament}>Decreament By Function</button>
+            <hr />
+
+            <button onClick={() => handdleTheme()}>{theme}</button>
         </div>
-  )
+    )
 }
 
 export default Counter
